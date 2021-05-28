3de7ae8e35913c15ea5415e0269cc332117fbb7a
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-05-28 14:14:14 -0300

    Identando funçoes e promises

--- projects/pokedex/app.js
+++ projects/pokedex/app.js
@@ -1,28 +1,30 @@
 const getPokemonURL = (id) => `https://pokeapi.co/api/v2/pokemon/${id}`;
 
-const fetchPokemon = () => {
-  
-  const pokemonPromises = [];
-  for (let i = 1; i <= 150; i += 1) {
-    pokemonPromises.push(
-      fetch(getPokemonURL(i)).then((response) => response.json())
-    );
-  }
-  Promise.all(pokemonPromises).then((pokemons) => {
-    const listPokemons = pokemons.reduce((acc, pokemon) => {
-      const types = pokemon.types.map((typeInfo) => typeInfo.type.name);
-      acc += `
-      <li class="card ${types[0]}">
-        <img class="card-image" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" alt="${pokemon.name}">
-        <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
-        <p class="card-subtitle">${types.join(' | ')}</p>
-      </li>
-      `
-      return acc;
-    }, '');
-    console.log(listPokemons);
-    document.querySelector('.pokedex').innerHTML = listPokemons;
-  });
-};
+const generatePokemonPromises = () => Array(150).fill().map((_, index) =>
+  fetch(getPokemonURL(index + 1)).then((response) => response.json())
+);
 
-fetchPokemon();
+
+const generateHTML = pokemons => pokemons.reduce((acc, { id, name, types }) => {
+    const elementTypes = types.map((typeInfo) => typeInfo.type.name);
+    acc += `
+      <li class="card ${elementTypes[0]}">
+        <img class="card-image" src="https://pokeres.bastionbot.org/images/pokemon/${id}.png" alt="${name}">
+        <h2 class="card-title">${id}. ${name}</h2>
+        <p class="card-subtitle">${elementTypes.join(' | ')}</p>
+      </li>
+    `
+  return acc;
+  }, '');
+
+const insertPokemonsInList = listPokemons => {
+  document.querySelector('.pokedex').innerHTML = listPokemons;
+}
+
+const pokemonPromises = generatePokemonPromises();
+
+Promise.all(pokemonPromises)
+  .then(generateHTML) 
+  .then(insertPokemonsInList);
+
+
