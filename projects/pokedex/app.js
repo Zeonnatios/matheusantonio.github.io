ff50a8c1872681d5443eb50b84dff2272db441fa
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-06-03 10:47:01 -0300

    Refazendo javaScript

--- projects/pokedex/app.js
+++ projects/pokedex/app.js
@@ -1,30 +1,35 @@
+const ul = document.querySelector('.pokedex');
+
 const getPokemonURL = (id) => `https://pokeapi.co/api/v2/pokemon/${id}`;
 
-const generatePokemonPromises = () => Array(150).fill().map((_, index) =>
-  fetch(getPokemonURL(index + 1)).then((response) => response.json())
-);
+const generatePokemonPromises = () =>
+  Array(150)
+    .fill()
+    .map((_, index) =>
+      fetch(getPokemonURL(index + 1)).then((response) => response.json())
+    );
 
-
-const generateHTML = pokemons => pokemons.reduce((acc, { id, name, types }) => {
+const generateElements = (pokemons) =>
+  pokemons.forEach(({ id, name, types }) => {
     const elementTypes = types.map((typeInfo) => typeInfo.type.name);
-    acc += `
-      <li class="card ${elementTypes[0]}">
-        <img class="card-image" src="https://pokeres.bastionbot.org/images/pokemon/${id}.png" alt="${name}">
-        <h2 class="card-title">${id}. ${name}</h2>
-        <p class="card-subtitle">${elementTypes.join(' | ')}</p>
-      </li>
-    `
-  return acc;
-  }, '');
-
-const insertPokemonsInList = listPokemons => {
-  document.querySelector('.pokedex').innerHTML = listPokemons;
-}
+    const li = document.createElement('li');
+    li.classList.add('card', `${elementTypes[0]}`);
+    const img = document.createElement('img');
+    img.src = `https://pokeres.bastionbot.org/images/pokemon/${id}.png`;
+    img.classList.add('card-image');
+    img.alt = `${name}`;
+    const h2 = document.createElement('h2');
+    h2.classList.add('card-title');
+    h2.innerHTML = `${id}. ${name}`;
+    const p = document.createElement('p');
+    p.classList.add('card-subtitle');
+    p.innerHTML = `${elementTypes.join(' | ')}`;
+    li.appendChild(img);
+    li.appendChild(h2);
+    li.appendChild(p);
+    ul.appendChild(li);
+  });
 
 const pokemonPromises = generatePokemonPromises();
 
-Promise.all(pokemonPromises)
-  .then(generateHTML) 
-  .then(insertPokemonsInList);
-
-
+Promise.all(pokemonPromises).then(generateElements);
