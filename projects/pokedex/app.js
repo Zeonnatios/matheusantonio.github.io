8e4af00d734b5e1b65bfcb37ff0446957c41d63d
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-06-03 13:22:19 -0300

    Refatorando js pokedex

--- projects/pokedex/app.js
+++ projects/pokedex/app.js
@@ -3,27 +3,33 @@ const ul = document.querySelector('.pokedex');
 const getPokemonURL = (id) => `https://pokeapi.co/api/v2/pokemon/${id}`;
 
 const generatePokemonPromises = () =>
-  Array(150)
-    .fill()
-    .map((_, index) =>
+    Array(150).fill().map((_, index) =>
       fetch(getPokemonURL(index + 1)).then((response) => response.json())
     );
+
+const createCustomElement = (tag, classList, innerHTML) => {
+  const element = document.createElement(tag);
+  element.classList.add(classList);
+  element.innerHTML = innerHTML;
+  return element;
+};
+
+const createImageElement = (id, classList, alt) => {
+  const img = document.createElement('img');
+  img.src = `https://pokeres.bastionbot.org/images/pokemon/${id}.png`;
+  img.className = classList;
+  img.alt = alt;
+  return img;
+};
 
 const generateElements = (pokemons) =>
   pokemons.forEach(({ id, name, types }) => {
     const elementTypes = types.map((typeInfo) => typeInfo.type.name);
     const li = document.createElement('li');
     li.classList.add('card', `${elementTypes[0]}`);
-    const img = document.createElement('img');
-    img.src = `https://pokeres.bastionbot.org/images/pokemon/${id}.png`;
-    img.classList.add('card-image');
-    img.alt = `${name}`;
-    const h2 = document.createElement('h2');
-    h2.classList.add('card-title');
-    h2.innerHTML = `${id}. ${name}`;
-    const p = document.createElement('p');
-    p.classList.add('card-subtitle');
-    p.innerHTML = `${elementTypes.join(' | ')}`;
+    const img = createImageElement(id, 'card-image', name);
+    const h2 = createCustomElement('h2', 'card-title', `${id}. ${name}`);
+    const p = createCustomElement('p', 'card-subtitle', `${elementTypes.join(' | ')}`);
     li.appendChild(img);
     li.appendChild(h2);
     li.appendChild(p);
